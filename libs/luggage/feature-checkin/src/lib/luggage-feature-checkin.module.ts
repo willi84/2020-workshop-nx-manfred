529dadace7cd61bf6022362afd415dfa1ce73d99
Robert Willemelis <willemelis@strato.de>
2020-10-20 11:00:44 +0200

    chore: safe state

--- libs/luggage/feature-checkin/src/lib/luggage-feature-checkin.module.ts
+++ libs/luggage/feature-checkin/src/lib/luggage-feature-checkin.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from "@angular/common";
 import { LuggageDomainModule } from '@flight-workspace/luggage/domain';
 import { CheckinComponent } from './checkin.component';
 import { RouterModule } from '@angular/router';
+import { LuggageUiCardModule  } from '@flight-workspace/luggage/ui-card';
 
 @NgModule({
   imports: [
     CommonModule, LuggageDomainModule,
+    LuggageUiCardModule,
     RouterModule.forChild([
       { path: '', component: CheckinComponent }
   ])
